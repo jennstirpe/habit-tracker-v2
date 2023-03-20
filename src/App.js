faa9ce575f6525676988ab57b345b537cb7d5a24
jstirpe <jennstirpe@yahoo.com>
2023-03-20 15:39:47 -0400

    Created separate Main component for readability. Refactored History component to remove if/else statements from return function

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from './themes.js';
 
 import Header from "./components/header/Header";
-import Checklist from "./components/main/display/Checklist";
-import History from "./components/main/display/History";
+import Main from "./components/main/Main";
 import HabitSetup from "./components/main/form/HabitSetup";
-import Streaks from "./components/main/display/Streaks";
 
 const LOCAL_STORAGE_KEY = "habitsapp.tracking"
 
@@ -248,28 +246,30 @@ function App() {
 
       <Header colorTheme={colorTheme} toggleTheme={toggleTheme} />
 
-      {
+      {/* {
         habits.length === 0 && <HabitSetup habitList={[]} createHabitList={createHabitList} setSetupFormActive={setSetupFormActive} />
+      } */}
+
+      {
+        habits.length === 0 ? <HabitSetup habitList={[]} createHabitList={createHabitList} setSetupFormActive={setSetupFormActive} /> : null
       }
 
       {
-        habits.length > 0 && <main>
-        <button onClick={() => setSetupFormActive(true)} className="global-edit-btn" aria-label="Add or edit habits">
-          <svg viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M343.029 42.3419C352.402 32.9693 367.598 32.9694 376.971 42.3419L469.657 135.028C479.029 144.401 479.029 159.597 469.657 168.969L179.462 459.164C179.006 459.633 178.521 460.073 178.011 460.482C176 462.099 173.688 463.158 171.283 463.66C170.898 463.741 170.509 463.808 170.115 463.86L50.1148 479.86C45.175 480.519 40.2103 478.838 36.6864 475.314C33.1626 471.79 31.4819 466.826 32.1405 461.886L48.1164 342.066C48.5312 338.637 50.0545 335.317 52.6863 332.685L343.029 42.3419ZM393.372 199.999L167.999 425.372L86.6267 344L312 118.627L393.372 199.999ZM416 177.372L441.373 151.999L360 70.6262L334.627 95.9992L416 177.372ZM75.5807 378.208L66.6251 445.376L133.792 436.42L75.5807 378.208Z" fill="black"/></svg>
-        </button>
-        {
-          setupFormActive && <HabitSetup habitList={habits} createHabitList={createHabitList} setSetupFormActive={setSetupFormActive} />
-        }
-        
-        <Checklist currentDay={records[0]} completeHabit={completeHabit} updateHabitQuantity={updateHabitQuantity} toggleStreaks={toggleStreaks} streaksActive={streaksActive} />
-        
-        {
-          streaksActive && <Streaks habits={habits} setStreaksActive={setStreaksActive} />
-        }
-        
-        
-        <History records={records} today={today} setStreaksActive={setStreaksActive} />
-      </main>
+        habits.length > 0 && <Main 
+        setSetupFormActive={setSetupFormActive} 
+        setupFormActive={setupFormActive} 
+        habitList={habits}
+        createHabitList={createHabitList}
+        currentDay={records[0]}
+        completeHabit={completeHabit}
+        updateHabitQuantity={updateHabitQuantity}
+        toggleStreaks={toggleStreaks}
+        streaksActive={streaksActive}
+        setStreaksActive={setStreaksActive}
+        records={records}
+        today={today}
+        />
+
       }
 
     </>
--- src/components/main/display/History.js
+++ src/components/main/display/History.js
@@ -2,54 +2,59 @@ import { StyledHistory } from "../styled/display/History.styled"
 
 export default function History({ records, today }) {
 
+// create blocks for display bar based on type of habit and whether complete or not
+    function createBlocks(record) {
+        const habitBlocks = record.habits.map(habit => {
+            if (habit.type === "check") {
+                if(habit.complete) {
+                    return <li className="display-bar-box" key={habit.id} style={{'background' : `${habit.color}`}}>
+                        <div className="display-bar-label check"><span className="label-name">{habit.name}</span></div>
+                    </li>
+                } else {
+                    return <li className="display-bar-box" key={habit.id}>
+                        <div className="display-bar-label check"><span className="label-name">{habit.name}</span></div>
+                    </li>
+                }
+            } else {
+                if (habit.goal.currentAmt === 0) {
+                    return <li className="display-bar-box" key={habit.id}>
+                        <div className="display-bar-label qty"><span className="label-name">{habit.name}</span><span className="label-amt-current">0</span> <span className="label-amt-goal">{habit.goal.goalAmt}</span></div>
+                    </li>
+                } else if (habit.goal.currentAmt >= habit.goal.goalAmt) {
+                    return <li className="display-bar-box" key={habit.id} style={{'background' : `${habit.color}`}}>
+                        <div className="display-bar-label qty"><span className="label-name">{habit.name}</span><span className="label-amt-current">{habit.goal.currentAmt}</span> <span className="label-amt-goal">{habit.goal.goalAmt}</span></div>
+                    </li>
+                } else {
+                    let percentageComplete = Math.round(habit.goal.currentAmt / habit.goal.goalAmt * 100);
+                    if (percentageComplete < 10) {
+                        percentageComplete = 10;
+                    }
+                    return <li className="display-bar-box" key={habit.id} style={{'background' : `${habit.color}${percentageComplete}`}}>
+                        <div className="display-bar-label qty"><span className="label-name">{habit.name}</span><span className="label-amt-current">{habit.goal.currentAmt}</span> <span className="label-amt-goal">{habit.goal.goalAmt}</span></div>
+                    </li>
+                }
+                
+            }
+        })
+
+        return habitBlocks;
+    }
+
 
   return (
+
     <StyledHistory>
         {
             records.map(record => {
                 return <li className="history-record" key={record.id}>
                         <span className="history-record-date">{record.date.toString() == today.toString() ? "Today" : `${record.date[1]}/${record.date[2]}/${record.date[0]}`}</span>
                         <ul className="history-record-display-bar">
-                            {
-                                record.habits.map(habit => {
-                                    if (habit.type === "check") {
-                                        if(habit.complete) {
-                                            return <li className="display-bar-box" key={habit.id} style={{'background' : `${habit.color}`}}>
-                                                <div className="display-bar-label check"><span className="label-name">{habit.name}</span></div>
-                                            </li>
-                                        } else {
-                                            return <li className="display-bar-box" key={habit.id}>
-                                                <div className="display-bar-label check"><span className="label-name">{habit.name}</span></div>
-                                            </li>
-                                        }
-                                    } else {
-                                        if (habit.goal.currentAmt === 0) {
-                                            return <li className="display-bar-box" key={habit.id}>
-                                                <div className="display-bar-label qty"><span className="label-name">{habit.name}</span><span className="label-amt-current">0</span> <span className="label-amt-goal">{habit.goal.goalAmt}</span></div>
-                                            </li>
-                                        } else if (habit.goal.currentAmt >= habit.goal.goalAmt) {
-                                            return <li className="display-bar-box" key={habit.id} style={{'background' : `${habit.color}`}}>
-                                                <div className="display-bar-label qty"><span className="label-name">{habit.name}</span><span className="label-amt-current">{habit.goal.currentAmt}</span> <span className="label-amt-goal">{habit.goal.goalAmt}</span></div>
-                                            </li>
-                                        } else {
-                                            let percentageComplete = Math.round(habit.goal.currentAmt / habit.goal.goalAmt * 100);
-                                            if (percentageComplete < 10) {
-                                                percentageComplete = 10;
-                                            }
-                                            return <li className="display-bar-box" key={habit.id} style={{'background' : `${habit.color}${percentageComplete}`}}>
-                                                <div className="display-bar-label qty"><span className="label-name">{habit.name}</span><span className="label-amt-current">{habit.goal.currentAmt}</span> <span className="label-amt-goal">{habit.goal.goalAmt}</span></div>
-                                            </li>
-                                        }
-                                        
-                                    }
-                                    
-                                })
-                            }
+                            { createBlocks(record) }
                         </ul>
                     </li>
             })
         }
-
     </StyledHistory>
+
   )
 }
